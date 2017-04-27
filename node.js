8b6cf17b3fea4f94abdd6aef9d6ebdb250beef49
mgumilang <muhammadgumilang11@gmail.com>
2017-04-27 14:23:39 +0700

    Add status and promise

--- node.js
+++ node.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = process.argv[2] || 3000
+const Promise = require('bluebird')
 console.log('Node started on port', port)
 
 const FOLLOWER = 0
@@ -9,15 +10,41 @@ const CANDIDATE = 1
 const LEADER = 2
 
 let status = FOLLOWER
+let resolver = null
+
+// Other nodes' status
+node_leader = null
+node_candidate = []
+node_follower = []
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   serverID = req.body.id
   serverCPU = req.body.cpu
   console.log(`Received from Server #${serverID}, Usage = ${serverCPU}`)
-  // TODO: Save it into log
-  // Also some timeout thing to determine if the server is dead
+  res.send('kontol')
+  resolver({ serverID: serverID, serverCPU: serverCPU })
 })
+
+function listen() {
+	return new Promise((resolve, reject) => {
+		resolver = resolve
+	})
+}
+
+function run() {
+	let listenTimeout = new Promise(function(resolve, reject) { 
+	    setTimeout(resolve, 5000, 'one')
+	})
+	
+	Promise.race([listen(), listenTimeout]).then(function(value) {
+		console.log('ANJING')
+		console.log(value)
+		run()
+	})
+}
+
+run()
 
 app.listen(port)
